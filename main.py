4ef3d00306aa872003008ad2e4d46504b4e6a4b1
robin <107808027+robinmerde@users.noreply.github.com>
2022-10-07 13:28:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -227,12 +227,11 @@ if __name__ == '__main__':
     while 1:
         try:
             choose()
+            next = random.randint(5400, 10080)
+            minutes, seconds = divmod(next, 60)
+            hours, minutes = divmod(minutes, 60)
+            print("Nouveau Tweet dans %d:%02d:%02d" % (hours, minutes, seconds))
+            time.sleep(next)
         except Exception:
-            print(Exception)
-            pass
-                
-        next = random.randint(5400, 10080)
-        minutes, seconds = divmod(next, 60)
-        hours, minutes = divmod(minutes, 60)
-        print("Nouveau Tweet dans %d:%02d:%02d" % (hours, minutes, seconds))
-        time.sleep(next)
+            continue
+        
